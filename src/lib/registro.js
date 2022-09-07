a3d8dfb64a9fa1dadb33139b5baa2b8bb2b18cd5
stefaniacamargo <estefaniacamargo263@gmail.com>
2022-09-07 15:31:18 -0500

    diseño  muro css

--- src/lib/registro.js
+++ src/lib/registro.js
@@ -2,8 +2,10 @@ import { registro, google } from './auth.js';
 
 export const resgContenido = `<section class="contenedor">
 <section class="logoInicial">
+  <div>
   <img class="titulo" src="img/tituloprincipal.PNG" alt="music book" />
   <img class="logo" src="img/Logo.png" alt="logo" />
+  </div>
 </section>
 <section class="registro">
 <form id="registro">
@@ -12,12 +14,12 @@ export const resgContenido = `<section class="contenedor">
   <label>Correo electrónico</label> <br>
   <input id="correo" type="text"> <br>
   <label>Contraseña</label> <br>
-  <input id="contraseña" type="text"> <br>
+  <input id="contraseña" type="password"> <br>
   <button type="submit" id="registrate">Regístrate</button> <br>
   </form>
-  <p>ó</p>
-  <button id="google">Continúa con Google</button>
-  <p>¿Tienes una cuenta? <a href="#iniciarSesion">Iniciar Sesión</a></p>
+  <img id="google" class="logo" src="img/botongoogle.png" alt="continua con google">
+  <p>¿Tienes una cuenta? </p>
+  <p><a href="#iniciarSesion">Iniciar Sesión</a></p>
 </section>
 </section>`;
 
